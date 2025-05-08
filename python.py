3c09988e85fb79791ff7ae0649495100c310d962
donghohoho <donghosim61@gmail.com>
2025-05-08 21:07:54 +0900

    modified:   python.py

--- python.py
+++ python.py
@@ -1,16 +1,23 @@
 from flask import Flask 
 app = Flask(__name__)
+topics = [
+    {'id':1, 'title' : 'html', 'body' :  'html is...'},
+    {'id':2, 'title' : 'css', 'body' :  'css is...'},
+    {'id':3, 'title' : 'javascript', 'body' :  'javascript is...'}
 
+
+]
 @app.route('/')
 def index():
-    return '''<doctype html>
+    liTags = ''
+    for topic in topics:
+        liTags = liTags+ f'<li><a href = "/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f'''<!doctype html>
     <html>
         <body>
-            <h1><a href = "/read/1/">html</a></li>
+            <h1><a href = "/">Web</a></h1>
             <ol>
-                <li><a href="read/1/">html</a></li>
-                <li><a href="read/2/">css</a></li>
-                <li><a href="read/3/">javascript</a></li>
+                {liTags}
             </ol>
             <h2>Welcome</h2>
             Hello, Web
