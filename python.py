8518eb7d0ae79ed17a190b3d4abfee007afec5a3
donghohoho <donghosim61@gmail.com>
2025-05-08 20:47:05 +0900

    modified:   python.py

--- python.py
+++ python.py
@@ -1,18 +1,17 @@
 from flask import Flask 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return '''<doctype html>
     <html>
         <body>
             <h1><a href = "/read/1/">html</a></li>
-            <o1>
+            <ol>
                 <li><a href="read/1/">html</a></li>
                 <li><a href="read/2/">css</a></li>
                 <li><a href="read/3/">javascript</a></li>
-            </o1>
+            </ol>
             <h2>Welcome</h2>
             Hello, Web
         </body>
@@ -21,13 +20,13 @@ def index():
 
 @app.route('/create/')
 def create():
-    return 'create'
+    return 'Create'
 
 @app.route('/read/<id>/')
 def read(id):
     print (id)
 
-    return 'read ' + id
+    return 'Read ' + id
 
 
 app.run(debug=True)
