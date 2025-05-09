7a643e847f1343826f87bc163d9bbc314a861e20
yuna118 <yuna01180@gmail.com>
2025-05-10 06:20:34 +0900

    Update main.dart

--- khuthon/lib/main.dart
+++ khuthon/lib/main.dart
@@ -1,67 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:khuthon/main_page.dart';
+import 'package:khuthon/splash.dart';
+import 'firebase_options.dart';
+import 'main_page.dart';
 
-class ChatRoomPage extends StatelessWidget {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('채팅방'),
-        actions: [IconButton(icon: Icon(Icons.more_vert), onPressed: () {})],
+    return MaterialApp(
+      title: '같이농장',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.all(10),
-              children: [
-                _messageBubble('안녕하세요!', isMe: false),
-                _messageBubble('네 반가워요~', isMe: true),
-              ],
-            ),
-          ),
-          _chatInputField(),
-        ],
-      ),
-    );
-  }
-
-  Widget _messageBubble(String text, {bool isMe = false}) {
-    return Align(
-      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 4),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: isMe ? Colors.green[100] : Colors.grey[200],
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Text(text),
-      ),
-    );
-  }
-
-  Widget _chatInputField() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      color: Colors.green[100],
-      child: Row(
-        children: [
-          Icon(Icons.add),
-          SizedBox(width: 8),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: '메시지 입력...',
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.send),
-            onPressed: () {},
-          ),
-        ],
-      ),
+      home: MainScreen(),
     );
   }
 }
