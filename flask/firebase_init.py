32e9ebfcaee686509218883873f74ca3fd1390e6
donghohoho <donghosim61@gmail.com>
2025-05-09 21:35:47 +0900

    modified:   flask/firebase_init.py

--- flask/firebase_init.py
+++
@@ -1,8 +0,0 @@
-# firebase_init.py
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
-
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
