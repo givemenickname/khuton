9d70ed9a0c1142459d466361c01e7447e5e84821
ktjdus <3217sy@khu.ac.kr>
2025-05-09 18:00:29 +0900

    test

--- khuthon/lib/main.dart
+++ khuthon/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Firebase 초기화 전 필수
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // ← 여기!
+  );
   runApp(const MyApp());
 }
 
