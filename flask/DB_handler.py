b929d98ca6fccbdbc4eb10f9256aa51b465c67f3
ktjdus <3217sy@khu.ac.kr>
2025-05-09 22:03:58 +0900

    db handler

--- flask/DB_handler.py
+++ flask/DB_handler.py
@@ -7,3 +7,21 @@ class DBModule:
             config = json.load(f)
 
         self.firebase = pyrebase.initialize_app(config)
+
+        def login(self, id, password):
+            pass
+
+        def signin(self, id, password, name, gender, birth, address, mbti, plant):
+            pass
+
+        def write_post(self, user, contents, capacity):
+            pass
+
+        def post_list(self):
+            pass
+
+        def post_detail(self, pid):
+            pass
+
+        def get_user(self, uid):
+            pass
